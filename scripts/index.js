4b27355e4e2315856498b5ccf91f2305c7d86dd9
Jevgenii <porozovwork@gmail.com>
2021-11-05 13:59:28 +0300

    Второе исправление замечаний вьюера 2

--- scripts/index.js
+++ scripts/index.js
@@ -21,6 +21,33 @@ const popupSaveButton = document.querySelector('.popup__save_type_profile');
 const cardTemplate = document.querySelector('#template-card').content;
 const cardsList = document.querySelector('.cards');
 
+const initialCards = [
+	{
+		name: 'Архыз',
+		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+	},
+	{
+		name: 'Челябинская область',
+		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+	},
+	{
+		name: 'Иваново',
+		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+	},
+	{
+		name: 'Камчатка',
+		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+	},
+	{
+		name: 'Холмогорский район',
+		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+	},
+	{
+		name: 'Байкал',
+		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+	}
+];
+
 const validationConfig = {
 	formSelector: '.popup__form',
 	inputSelector: '.popup__input',
