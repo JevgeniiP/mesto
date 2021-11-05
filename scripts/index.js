eb55833e9ff6873e285a2dbe2be3181d41d6a968
Jevgenii <porozovwork@gmail.com>
2021-11-05 13:53:37 +0300

    Второе исправление замечаний вьюера

--- scripts/index.js
+++ scripts/index.js
@@ -21,32 +21,13 @@ const popupSaveButton = document.querySelector('.popup__save_type_profile');
 const cardTemplate = document.querySelector('#template-card').content;
 const cardsList = document.querySelector('.cards');
 
-const initialCards = [
-	{
-		name: 'Архыз',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-	},
-	{
-		name: 'Челябинская область',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-	},
-	{
-		name: 'Иваново',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-	},
-	{
-		name: 'Камчатка',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-	},
-	{
-		name: 'Холмогорский район',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-	},
-	{
-		name: 'Байкал',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-	}
-];
+const validationConfig = {
+	formSelector: '.popup__form',
+	inputSelector: '.popup__input',
+	inputErrorClass: 'popup__input_state_invalid',
+	submitButtonSelector: '.popup__save',
+	submitButtonErrorClass: 'popup__save_invalid',
+};
 
 window.onload = function () {
 	popupEditProfile.classList.remove('preload');
@@ -100,7 +81,7 @@ function closePopup(popup) {
 
 function popupClickHandler(event, popup) {
 	if (event.target.classList.contains('popup')) {
-		closePopup(document.querySelector('.popup_is-open'));
+		closePopup(popup);
 	}
 };
 
@@ -133,7 +114,7 @@ function submitCardsForm(event) {
 	closePopup(popupAddCards);
 	refreshInputForm(inputCardName);
 	refreshInputForm(inputCardUrl);
-	buttonState(buttonCreateCards);
+	setSubmitButtonState(formCards, validationConfig);
 }
 
 editButton.addEventListener('click', () => {
@@ -153,8 +134,6 @@ formCards.addEventListener('submit', submitCardsForm);
 
 popupFullscreenClose.addEventListener('click', () => { closePopup(popupFullscreen) });
 
-popupFullscreen.addEventListener('mouseup', () => { popupClickHandler(event, popupFullscreen) });
-popupEditProfile.addEventListener('mouseup', () => { popupClickHandler(event, popupEditProfile) });
-popupAddCards.addEventListener('mouseup', () => { popupClickHandler(event, popupAddCards) });
-
-
+popupFullscreen.addEventListener('click', () => { popupClickHandler(event, popupFullscreen) });
+popupEditProfile.addEventListener('click', () => { popupClickHandler(event, popupEditProfile) });
+popupAddCards.addEventListener('click', () => { popupClickHandler(event, popupAddCards) });
